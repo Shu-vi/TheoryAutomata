fe9ba8e8f6dabad30842f14f8b63ed726c570811
Vadim <vad.general@mail.ru>
2021-11-30 00:59:36 +0300

    Пример автомата для 4 задания

--- src/Task4/TestDrive.java
+++ src/Task4/TestDrive.java
@@ -15,9 +15,22 @@ public class TestDrive {
                 {"7","6","8"},
                 {"8","6","8"}
         };
+        //String[][] t = {
+//                {" ","a","b"},
+//                {"1","3","2"},
+//                {"2","3","1"},
+//                {"3","4","4"},
+//                {"4","8","5"},
+//                {"5","5","5"},
+//                {"6","6","6"},
+//                {"7","8","5"},
+//                {"8","5","6"}
+//        };
         ArrayList<String> newEq = new ArrayList<>();
         newEq.add("356");
         newEq.add("12478");
+        //newEq.add("56");
+//        newEq.add("123478");
         DFA dfa = new DFA(t, newEq);
         dfa.minimize();
     }
